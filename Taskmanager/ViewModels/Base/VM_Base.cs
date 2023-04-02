b436e16fb34e9a3237d59f370d5cd7b48ebcdd34
Bose-fuchs <bosefuchs@gmail.com>
2023-04-03 00:46:19 +0300

    Создана базовая ViewModel

---
+++ Taskmanager/ViewModels/Base/VM_Base.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taskmanager.ViewModels.Base
+{
+    // Абстрактный класс, реализующий интерфейс INotifyPropertyChanged
+    internal abstract class VM_Base : INotifyPropertyChanged
+    {
+        // Событие, уведомляющее о изменении свойства
+        public event PropertyChangedEventHandler PropertyChanged;
+        // Метод вызова события PropertyChanged
+        // [CallerMemberName] - (именно запись - атрибут для компилятора) синтаксический "сахар", позволяющий не указывать параметр
+        protected virtual void OnPropertyChanged([CallerMemberName] string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        // Обновление значения свойства, для которого определено поле хранения данных? || ХЗ зачем, 48 минута видоса 
+        protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string PropertyName = null)
+        { 
+            if (Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(PropertyName);
+            return true;
+        }
+
+    }
+}
---
+++ Taskmanager/ViewModels/ViewModelTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Taskmanager.ViewModels
+{
+    class ViewModelTest:INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+
+        private int _Cliks;
+        public int Cliks 
+        { 
+            get { return _Cliks; }
+            set 
+            {
+                _Cliks = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ViewModelTest()
+        {
+            Task.Factory.StartNew(() =>
+            {
+                while (true)
+                {
+                    Task.Delay(1000).Wait();
+
+                    Cliks++;
+                }
+            });
+        }
+
+
+    }
+}
